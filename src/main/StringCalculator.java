52179202617043a9d0430b41dacb0d6155e0f0b5
Gummi St <gummi@climbing.is>
2016-10-14 19:52:01 +0000

    fixed the zero and test ok

--- src/main/StringCalculator.java
+++ src/main/StringCalculator.java
@@ -3,6 +3,10 @@ package ru.Stringcalculator;
 
 public class StringCalculator{
 	public static int add(String text){
+		if (text.equals("")) {
+			return 0;
+		}
+
 		return Integer.MIN_VALUE;
 	}
 }
